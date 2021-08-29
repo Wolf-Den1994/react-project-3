38b9c6f996620c4065d766e26866876707494cd4
Den <wolf-den@tut.by>
2021-08-29 14:26:56 +0300

    feat: HOOKS useState data

--- src/workWithHooks.js
+++ src/workWithHooks.js
@@ -3,11 +3,24 @@ import './workWithHooks.scss';
 
 const NewApp = () => {
   const [count, setCount] = useState(0);
+  const [data, refreshData] = useState([{ name: 'Den', sex: 'male' }]);
 
   return (
     <div className="hooooks">
       <p>Вы кликнули {count} раз</p>
       <button onClick={() => setCount(count + 1)}>Кликни меня</button>
+      {data.map((item) => (
+        <div>
+          Name: {item.name}, sex: {item.sex}
+        </div>
+      ))}
+      <button
+        onClick={() =>
+          refreshData((data) => [...data, { name: 'Jhon', sex: 'male' }])
+        }
+      >
+        Добавить данные
+      </button>
     </div>
   );
 };
